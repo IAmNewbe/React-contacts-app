efc95cdec431497c4b32f316067b06c09c956cb8
IAmNewbe <ahlulhikam2222@gmail.com>
2023-04-18 06:36:06 +0700

    Final Release

--- src/components/ContactApp.js
+++ src/components/ContactApp.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { getData } from "../utils/data";
 import ContactList from "./ContactList";
+import ContactInput from "./ContactInput";
 
 
 class ContactApp extends React.Component {
@@ -12,6 +13,7 @@ class ContactApp extends React.Component {
         };
 
         this.onDeleteEventHandler = this.onDeleteEventHandler.bind(this);
+        this.onAddContactHandler = this.onAddContactHandler.bind(this);
     }
 
     onDeleteEventHandler(id){
@@ -19,9 +21,28 @@ class ContactApp extends React.Component {
         this.setState({contacts});
     }
 
+    onAddContactHandler({name, tag}) {
+        this.setState((prevState) => {
+            return {
+                contacts: [
+                    ...prevState.contacts,
+                    {
+                        id: +new Date(),
+                        name,
+                        tag,
+                        imageUrl: '/images/default.jpg',
+                    }
+                ]
+            }
+        });
+    }
+
     render() {
         return(
             <div className="contact-app">
+                <h1>Aplikasi Kontak</h1>
+                <h2>Tambah Kontak</h2>
+                <ContactInput addContact={this.onAddContactHandler}/>
                 <h1>Daftar Kontak</h1>
                 <ContactList contacts={this.state.contacts} onDelete={this.onDeleteEventHandler}/>
             </div>
---
+++ src/lab.js
@@ -0,0 +1,4 @@
+
+const arr = [1.2,343,546,3,6,5]
+
+console.log(arr);
